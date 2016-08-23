e3d0a09d5d5ee748da13740ac9fcb37706e41a92
Klaus Heino <klaus.heino@gmail.com>
2016-08-24 01:03:31 +0300

    Print dynamic data in each MovieListItem component.

--- src/components/movie_item.js
+++ src/components/movie_item.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const MovieListItem = (props) => {
-    return <li>Movie</li>;
+    const movie = props.movie;
+
+    //console.log('moii',movie);
+
+    return <a href="#" className="list-group-item" >{movie.Title}<span className="badge">{movie.Year}</span></a>;
 };
 
 export default MovieListItem;
