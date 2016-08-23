b286c5dc8fd0b0c3c8cd19f84b02ac23486d61bf
Klaus Heino <klaus.heino@gmail.com>
2016-08-24 01:57:40 +0300

    place the imdbID value in the variable

--- src/components/movie_item.js
+++ src/components/movie_item.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const MovieListItem = (props) => {
     const movie = props.movie;
-
+    const movieID = movie.imdbID;
+    
     //console.log('moii',movie);
 
     return <a href="#" className="list-group-item" >{movie.Title}<span className="badge">{movie.Year}</span></a>;
