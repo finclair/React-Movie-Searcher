c7a6588def3b17cbd6a68b4c7233598ea1bbfbdd
Klaus Heino <klaus.heino@gmail.com>
2016-08-24 13:57:23 +0300

    Add consturctor and markup to a component.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+
+
     render() {
-        return <input onChange = {this.onInputChange} />;
+        return (
+        <div className="well">
+            <form id="search">
+                <input onChange = {this.onInputChange} />
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" type="submit">
+                        <span class="glyphicon glyphicon-search"></span> Search
+                    </button>
+                </span>
+            </form>
+
+        </div>
+        );
     }
 
     onInputChange(event) {
         
     }
 }
-
 export default SearchBar;
+
+
