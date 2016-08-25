5f938822474ef8de8f914e22d129431dfa0bbffc
Klaus Heino <klaus.heino@gmail.com>
2016-08-25 22:23:34 +0300

    Add functionality for SearchBar component.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,29 +3,34 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     constructor(props) {
         super(props);
+        this.state = {input: ''};
 
-
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
-
 
     render() {
         return (
-        <div className="well">
-            <form className="search">
-                <input onChange = {this.onInputChange} />
-                <span className="input-group-btn">
-                    <button className="btn btn-primary" type="submit">
-                        <span className="glyphicon glyphicon-search"></span> Search
-                    </button>
-                </span>
-            </form>
-
-        </div>
+            <div className="well">
+                <form className="search" onSubmit={this.onFormSubmit} >
+                    <input value={this.state.input} onChange={this.onInputChange}/>
+                    <span className="input-group-btn">
+                        <button className="btn btn-primary" type="submit">
+                            <span className="glyphicon glyphicon-search" /> Search
+                        </button>
+                    </span>
+                </form>
+            </div>
         );
     }
 
     onInputChange(event) {
-        
+        this.setState({input: event.target.value});
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        this.props.onSearching(this.state.input);
     }
 }
 export default SearchBar;
