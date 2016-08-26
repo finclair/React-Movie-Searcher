be89f59591d626bbb47f1f0ec8be0cf06c465bae
Klaus Heino <klaus.heino@gmail.com>
2016-08-26 14:25:44 +0300

    Change the name of the function.

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ class Application extends Component {
   constructor(props) {
     super(props);
 
-    this.test = this.test.bind(this)
+    this.prepareSearchWord = this.prepareSearchWord.bind(this)
 
     this.state = { movies: [] }; //the state of movies starts as an empty array
 
@@ -60,7 +60,6 @@ class Application extends Component {
     this.setState({movies: movies.Search});
     }
     );
-
   }
 
   render() {
