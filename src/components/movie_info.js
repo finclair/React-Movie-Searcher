7f97d7e0e7b0c64a27a98dfc905a4fd39f96cef7
Klaus Heino <klaus.heino@gmail.com>
2016-08-26 21:08:59 +0300

    Set up component MovieInfo.

---
+++ src/components/movie_info.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const MovieInfo = (props) => {
+
+    console.log(props.movie);
+    const movie = props.movie;
+
+    const movieID = movie.imdbID;
+    //ToDo: should the baseurl http://www.omdbapi.com/?t= be here?
+
+    return (
+
+        <div className="col-md-5">
+            <div className="movie-info">
+                <article>
+                    <h2 className={movie.Title}></h2>
+                    <h4 className="year-of-movie"></h4>
+                    <p><span className="rate-of-movie" class="label label-default"></span></p>
+                    <p className="director-of-movie"></p>
+                    <p className="actors-of-movie"></p>
+                    <p><span className="imdb-rate-of-movǥie"></span></p>
+                    <p className="votes-of-movie"></p>
+                    <p className="oscars-of-movie"></p>
+                    <p className="plot-of-movie"></p>
+                </article>
+            </div>
+            <div className="movie-image"></div>
+        </div>
+  );
+};
+
+export default MovieInfo;
