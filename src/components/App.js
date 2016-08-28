d8a6bb4004aa57add3d333d2a733cc00c68f0a5d
Klaus Heino <klaus.heino@gmail.com>
2016-08-28 19:05:31 +0300

    Change the parent where all elements are rendered in index.js. Move the code of App component to its own file from index.js.

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,65 @@ import React, { Component } from 'react';
 
 require('../../style/style.css');
 
-export default class App extends Component {
+import SearchBar from './search_bar';
+import MovieList from './movie_list';
+import MovieInfo from './movie_info';
+
+//Create a component. This component should produce some HTML
+class Application extends Component {
+  constructor(props) {
+    super(props);
+
+    this.prepareSearchWord = this.prepareSearchWord.bind(this)
+    this.doDetailedSearch = this.doDetailedSearch.bind(this)
+
+    this.state = { movies: [] };
+
+  }
+
+  fetchOMDbData(url, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = () => {
+      if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        var response = JSON.parse(httpRequest.responseText);
+        callback(response);
+      }
+    };
+    httpRequest.open('GET', url);
+    httpRequest.send();
+  }
+
+  prepareSearchWord(input) {
+
+    this.fetchOMDbData(input, (movies) => {
+          this.setState({movies: movies.Search});
+          this.setState({selectedMovie: movies.Search[0]});
+        }
+    );
+  }
+  doDetailedSearch(input) {
+    this.fetchOMDbData(input, (selectedMovie) => {
+          this.setState({selectedMovie: selectedMovie});
+        }
+
+    );
+
+  }
   render() {
     return (
-      <div>
-        Hello world! kokeilua kokeilua
-      </div>
+        <div>
+          <div className="row">
+            <div className="col-md-7">
+              <SearchBar onSearching={this.prepareSearchWord} />
+              <MovieList
+                  movies={this.state.movies}
+                  onMovieClick={this.doDetailedSearch }
+              />
+            </div>
+            <MovieInfo movie={this.state.selectedMovie} />
+          </div>
+        </div>
     );
   }
 }
+export default Application;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
-import SearchBar from './components/search_bar';
-import MovieList from './components/movie_list';
-import MovieInfo from './components/movie_info';
 
 import reducers from './reducers';
 
@@ -23,68 +20,9 @@ if (module.hot) {
 }
 
 
-//Create a component. This component should produce some HTML
-class Application extends Component {
-  constructor(props) {
-    super(props);
-
-    this.prepareSearchWord = this.prepareSearchWord.bind(this)
-    this.doDetailedSearch = this.doDetailedSearch.bind(this)
-
-    this.state = { movies: [] };
-
-  }
-
-  fetchOMDbData(url, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = () => {
-      if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-        var response = JSON.parse(httpRequest.responseText);
-        callback(response);
-      }
-    };
-    httpRequest.open('GET', url);
-    httpRequest.send();
-  }
-
-  prepareSearchWord(input) {
-
-    this.fetchOMDbData(input, (movies) => {
-      this.setState({movies: movies.Search});
-      this.setState({selectedMovie: movies.Search[0]});
-    }
-    );
-  }
-  doDetailedSearch(input) {
-    this.fetchOMDbData(input, (selectedMovie) => {
-      this.setState({selectedMovie: selectedMovie});
-    }
-
-    );
-
-  }
-  render() {
-    return (
-      <div>
-        <div className="row">
-        <div className="col-md-7">
-          <SearchBar onSearching={this.prepareSearchWord} />
-          <MovieList
-              movies={this.state.movies}
-              onMovieClick={this.doDetailedSearch }
-          />
-        </div>
-          <MovieInfo movie={this.state.selectedMovie} />
-
-        </div>
-    </div>
-    );
-  }
-}
-
 //Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(
   //<Provider store={store}>
-    <Application />
+    <App />
   //</Provider>
-  , document.querySelector('.content'));
+  , document.querySelector('#app'));
